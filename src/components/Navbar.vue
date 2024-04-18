cfa4e41ae2f6610904fab76110c646b2d7e1cf6f
LeeJungHoon <gns14585@naver.com>
2024-04-18 11:43:17 +0900

    프로젝트 생성 , prettier, scss, fontawesome 설치

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar">
+    <button class="toggle">toggle</button>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.navbar {
+  background: #fff;
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  justify-content: end;
+}
+</style>
